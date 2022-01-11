1e6037502127c7d97098dfec122b46bf5c1bbe3b
pangnem <sprnd645@gmail.com>
2022-01-11 10:17:42 +0900

    fix: 소스 버튼 셀렉터 변경 반영

--- src/selectors/main.js
+++ src/selectors/main.js
@@ -4,7 +4,7 @@ const uri = '#edit-field-link-0-uri';
 const data = '#cke_1_contents > iframe';
 const openDataCategoryDropdown = '#edit-field-data-category-shs-0-0';
 const openDataType = '#edit-field-data-category-shs-0-1';
-const sourceButton = '#cke_22_label';
+const sourceButton = '#cke_23_label';
 const dataTextArea = '#cke_1_contents > textarea';
 const saveButton = '#edit-submit';
 
